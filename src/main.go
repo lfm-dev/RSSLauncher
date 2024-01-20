b8abb8c37490e04266c182c54a4f3f3c1abeb13c
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-20 18:37:44 -0300

    getFeed

--- src/main.go
+++ src/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/mmcdole/gofeed"
 )
 
-func main() {
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://calnewport.com/blog/feed")
+func getFeed(feedUrl string, fp *gofeed.Parser) *gofeed.Feed {
+	feed, err := fp.ParseURL(feedUrl)
 	if err != nil {
-		fmt.Println("oops")
+		fmt.Printf("Can't get %s data\n", feedUrl)
 	}
-	fmt.Println(feed.Title)
+	return feed
+}
+
+func main() {
+	feedsUrls := make([]string, 0)
+	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
+	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
+	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
+
+	fp := gofeed.NewParser()
+	for feedIndex, feedUrl := range feedsUrls {
+		feed := getFeed(feedUrl, fp)
+		lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
+		fmt.Println()
+		fmt.Println(feedIndex, feed.Title, lastUpdate)
+		for index, item := range feed.Items {
+			fmt.Println(index, item.Title, item.PublishedParsed.Format(time.UnixDate))
+		}
+	}
 }
