8f242d4d276e97555a943f993413751905a744b4
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-24 14:39:02 -0300

    getFeedData fn

--- src/main.go
+++ src/main.go
@@ -13,20 +13,8 @@ func getFeed(feedUrl string, fp *gofeed.Parser) (*gofeed.Feed, error) {
 	return feed, err
 }
 
-func main() {
-	feedsUrls := make([]string, 0)
-	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
-	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
-	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
-
-	fp := gofeed.NewParser()
-	for feedIndex, feedUrl := range feedsUrls {
-		feed, err := getFeed(feedUrl, fp)
-		if err != nil {
-			fmt.Printf("Can't get %s data\n", feedUrl)
-			continue
-		}
-
+func getFeedData(feeds []*gofeed.Feed) {
+	for feedIndex, feed := range feeds {
 		lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
 		fmt.Println()
 		fmt.Println(feedIndex, feed.Title, lastUpdate)
@@ -34,4 +22,26 @@ func main() {
 			fmt.Println(item.Title, item.PublishedParsed.Format("02-01-2006"))
 		}
 	}
+}
+
+func main() {
+	feedsUrls := make([]string, 0)
+	feeds := make([]*gofeed.Feed, 0)
+
+	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
+	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
+	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
+
+	fp := gofeed.NewParser()
+	for _, feedUrl := range feedsUrls {
+		feed, err := getFeed(feedUrl, fp)
+		if err != nil {
+			fmt.Printf("Can't get %s data\n", feedUrl)
+			continue
+		}
+		feeds = append(feeds, feed)
+	}
+
+	getFeedData(feeds)
+
 }
