d93d1147b267fa4589ef04a3c355c500e2272d74
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-24 17:50:57 -0300

    Revert "getFeedData fn"
    This reverts commit 8f242d4d276e97555a943f993413751905a744b4.

--- src/main.go
+++ src/main.go
@@ -13,8 +13,20 @@ func getFeed(feedUrl string, fp *gofeed.Parser) (*gofeed.Feed, error) {
 	return feed, err
 }
 
-func getFeedData(feeds []*gofeed.Feed) {
-	for feedIndex, feed := range feeds {
+func main() {
+	feedsUrls := make([]string, 0)
+	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
+	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
+	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
+
+	fp := gofeed.NewParser()
+	for feedIndex, feedUrl := range feedsUrls {
+		feed, err := getFeed(feedUrl, fp)
+		if err != nil {
+			fmt.Printf("Can't get %s data\n", feedUrl)
+			continue
+		}
+
 		lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
 		fmt.Println()
 		fmt.Println(feedIndex, feed.Title, lastUpdate)
@@ -22,26 +34,4 @@ func getFeedData(feeds []*gofeed.Feed) {
 			fmt.Println(item.Title, item.PublishedParsed.Format("02-01-2006"))
 		}
 	}
-}
-
-func main() {
-	feedsUrls := make([]string, 0)
-	feeds := make([]*gofeed.Feed, 0)
-
-	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
-	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
-	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
-
-	fp := gofeed.NewParser()
-	for _, feedUrl := range feedsUrls {
-		feed, err := getFeed(feedUrl, fp)
-		if err != nil {
-			fmt.Printf("Can't get %s data\n", feedUrl)
-			continue
-		}
-		feeds = append(feeds, feed)
-	}
-
-	getFeedData(feeds)
-
 }
