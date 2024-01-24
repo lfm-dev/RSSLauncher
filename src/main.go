1ef70cc9a54400326df2c9e56149341021c069ce
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-24 17:48:28 -0300

    oops

--- src/main.go
+++ src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/mmcdole/gofeed"
+	"golang.org/x/net/html"
 )
 
 //TODO run command with item url
@@ -20,18 +22,34 @@ func getFeedData(feeds []*gofeed.Feed) {
 		fmt.Println(feedIndex, feed.Title, lastUpdate)
 		for _, item := range feed.Items {
 			fmt.Println(item.Title, item.PublishedParsed.Format("02-01-2006"))
+			fmt.Println(item.Content)
+
+			doc, err := html.Parse(strings.NewReader(item.Content))
+			if err != nil {
+				fmt.Println("Error parsing HTML:", err)
+				return
+			}
+
+			fmt.Printf("Parsed HTML document: %+v\n", doc)
+
 		}
 	}
 }
 
+// func renderNode(n *html.Node) string {
+// 	var buf bytes.Buffer
+// 	w := io.Writer(&buf)
+// 	html.Render(w, n)
+// 	return buf.String()
+// }
+
 func main() {
 	feedsUrls := make([]string, 0)
-	feeds := make([]*gofeed.Feed, 0)
-
 	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
 	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
 	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
 
+	feeds := make([]*gofeed.Feed, 0)
 	fp := gofeed.NewParser()
 	for _, feedUrl := range feedsUrls {
 		feed, err := getFeed(feedUrl, fp)
@@ -41,7 +59,5 @@ func main() {
 		}
 		feeds = append(feeds, feed)
 	}
-
 	getFeedData(feeds)
-
 }
