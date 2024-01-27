d4e9f9029ea972d210857e0dd0941a99ed81b0ed
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-27 18:06:07 -0300

    playing with tview

--- go.mod
+++ go.mod
@@ -9,17 +9,17 @@ require (
 	github.com/anaskhan96/soup v1.2.5 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
-	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73 // indirect
+	github.com/gdamore/tcell/v2 v2.7.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mmcdole/gofeed v1.2.1 // indirect
 	github.com/mmcdole/goxpp v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/rivo/uniseg v0.4.3 // indirect
 	golang.org/x/net v0.6.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
-	"github.com/anaskhan96/soup"
+	"github.com/gdamore/tcell/v2"
 	"github.com/mmcdole/gofeed"
+	"github.com/rivo/tview"
 )
 
 //TODO run command with item url
@@ -21,28 +21,59 @@ func main() {
 	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
 	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
 
-	fp := gofeed.NewParser()
-	for feedIndex, feedUrl := range feedsUrls {
-		feed, err := getFeed(feedUrl, fp)
-		if err != nil {
-			fmt.Printf("Can't get %s data\n", feedUrl)
-			continue
-		}
+	// fp := gofeed.NewParser()
+	// for feedIndex, feedUrl := range feedsUrls {
+	// 	feed, err := getFeed(feedUrl, fp)
+	// 	if err != nil {
+	// 		fmt.Printf("Can't get %s data\n", feedUrl)
+	// 		continue
+	// 	}
 
-		lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
-		fmt.Println()
-		fmt.Println(feedIndex, feed.Title, lastUpdate)
-		for _, item := range feed.Items {
+	// 	lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
+	// 	fmt.Println()
+	// 	fmt.Println(feedIndex, feed.Title, lastUpdate)
+	// 	for _, item := range feed.Items {
 
-			fmt.Println("\n"+item.Title, item.PublishedParsed.Format("02-01-2006")+"\n")
+	// 		fmt.Println("\n"+item.Title, item.PublishedParsed.Format("02-01-2006")+"\n")
 
-			doc := soup.HTMLParse(item.Content)
-			fullText := strings.Split(doc.FullText(), "\n")
-			for _, line := range fullText {
-				if len(strings.TrimSpace(line)) != 0 {
-					fmt.Println(line)
-				}
-			}
-		}
+	// 		doc := soup.HTMLParse(item.Content)
+	// 		fullText := strings.Split(doc.FullText(), "\n")
+	// 		for _, line := range fullText {
+	// 			if len(strings.TrimSpace(line)) != 0 {
+	// 				fmt.Println(line)
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	app := tview.NewApplication()
+	table := tview.NewTable().
+		SetSelectable(true, false)
+
+	_, rows := 2, len(feedsUrls)
+	for row := 0; row < rows; row++ {
+		table.SetCell(row, 0,
+			tview.NewTableCell(feedsUrls[row]).
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignLeft))
+
+		table.SetCell(row, 1,
+			tview.NewTableCell("").
+				SetTextColor(tcell.ColorWhite).
+				SetAlign(tview.AlignCenter))
 	}
+
+	table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEscape {
+			app.Stop()
+		}
+	}).SetSelectedFunc(func(row int, column int) {
+		fmt.Println(table.GetCell(row, column).Text)
+		fmt.Println(feedsUrls[row])
+	})
+
+	if err := app.SetRoot(table, true).SetFocus(table).Run(); err != nil {
+		panic(err)
+	}
+
 }
