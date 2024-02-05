cee2fd94eae3a945866f2b65e5142b125a5d7f6d
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-05 17:01:49 -0300

    Feed & FeedItem

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/mmcdole/gofeed"
@@ -11,33 +12,69 @@ import (
 //TODO run command with item url
 //TODO open item on browser on enter
 
-func getFeed(feedUrl string, fp *gofeed.Parser) (*gofeed.Feed, error) {
-	feed, err := fp.ParseURL(feedUrl)
-	return feed, err
+type Feed struct {
+	url        string
+	name       string
+	lastUpdate time.Time
+	items      []FeedItem
+}
+
+type FeedItem struct {
+	url          string
+	title        string
+	date         time.Time
+	dateFormated string
+}
+
+func getFeedItems(goFeed *gofeed.Feed) []FeedItem {
+	feedItems := make([]FeedItem, 0)
+	for _, item := range goFeed.Items {
+		feedItem := FeedItem{
+			url:          item.Link,
+			title:        item.Title,
+			date:         *item.PublishedParsed,
+			dateFormated: item.PublishedParsed.Format("02-01-2006"),
+		}
+		feedItems = append(feedItems, feedItem)
+	}
+
+	return feedItems
+}
+
+func getFeeds(feedsUrls []string) []Feed {
+	feeds := make([]Feed, 0)
+
+	feedParser := gofeed.NewParser()
+	for i, feedUrl := range feedsUrls {
+		goFeed, err := feedParser.ParseURL(feedUrl)
+		if err != nil {
+			fmt.Printf("Can't get %s data\n", feedUrl)
+			continue
+		}
+
+		feedItems := getFeedItems(goFeed)
+		feed := Feed{
+			url:        feedsUrls[i],
+			name:       goFeed.Title,
+			lastUpdate: feedItems[0].date, // date of newest post
+			items:      feedItems,
+		}
+		feeds = append(feeds, feed)
+	}
+
+	return feeds
 }
 
 func main() {
-	// feedsUrls := make([]string, 0)
-	// feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
-	// feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
-	// feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
+	feedsUrls := make([]string, 0)
+	feedsUrls = append(feedsUrls, "https://www.raptitude.com/feed")
+	feedsUrls = append(feedsUrls, "https://go.dev/blog/feed.atom")
+	feedsUrls = append(feedsUrls, "https://calnewport.com/blog/feed")
 
-	// fp := gofeed.NewParser()
-	// for feedIndex, feedUrl := range feedsUrls {
-	// 	feed, err := getFeed(feedUrl, fp)
-	// 	if err != nil {
-	// 		fmt.Printf("Can't get %s data\n", feedUrl)
-	// 		continue
-	// 	}
+	feeds := getFeeds(feedsUrls)
+	fmt.Println(feeds)
 
-	// 	lastUpdate := feed.Items[0].PublishedParsed // update date of newest post
-	// 	fmt.Println()
-	// 	fmt.Println(feedIndex, feed.Title, lastUpdate)
-	// 	for _, item := range feed.Items {
-	// 		fmt.Println(item.Title, item.PublishedParsed.Format("02-01-2006"), item.Link)
-	// 	}
-	// }
-
+	//tview
 	app := tview.NewApplication()
 	table := tview.NewTable().
 		SetSelectable(true, false)
@@ -61,7 +98,7 @@ func main() {
 		}
 	}).SetSelectedFunc(func(row int, column int) {
 		fmt.Println(table.GetCell(row, column).Text)
-		fmt.Println(feedsUrls[row])
+		fmt.Println("ea", feedsUrls[row])
 	})
 
 	if err := app.SetRoot(table, true).SetFocus(table).Run(); err != nil {
