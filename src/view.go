ec2b22c17951e86e2a1d4e34905115b777c9a28c
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-17 13:56:40 -0300

    change posts table

--- src/view.go
+++ src/view.go
@@ -5,25 +5,15 @@ import (
 	"github.com/rivo/tview"
 )
 
-func view(feeds []Feed) {
+var (
+	mainFlex  = tview.NewFlex()
+	feedsFlex = tview.NewFlex()
+	postsFlex = tview.NewFlex()
+)
 
-	app := tview.NewApplication()
+func getFeedsTable(feeds []Feed) *tview.Table {
 	feedsTable := tview.NewTable().
 		SetSelectable(true, false)
-
-	var mainFlex = tview.NewFlex()
-	var feedsFlex = tview.NewFlex()
-	var postsFlex = tview.NewFlex()
-
-	feedsFlex.AddItem(feedsTable, 0, 1, false)
-	feedsFlex.SetBorder(true)
-	mainFlex.AddItem(feedsFlex, 0, 1, false)
-
-	postsTable := tview.NewTable().SetCell(0, 0,
-		tview.NewTableCell("hola"))
-	postsFlex.AddItem(postsTable, 0, 1, false)
-	postsFlex.SetBorder(true)
-	mainFlex.AddItem(postsFlex, 0, 2, false)
 
 	rows := len(feeds)
 	for row := 0; row < rows; row++ {
@@ -32,15 +22,31 @@ func view(feeds []Feed) {
 				SetTextColor(tcell.ColorWhite).
 				SetAlign(tview.AlignLeft))
 	}
+	return feedsTable
+}
+
+func makePostsTable(postsTable *tview.Table, feedIndex int) {
+	postsTable.SetCell(0, 0, tview.NewTableCell("test2"))
+}
+
+func view(feeds []Feed) {
+
+	app := tview.NewApplication()
+	feedsTable := getFeedsTable(feeds)
+	postsTable := tview.NewTable()
+
+	feedsFlex.AddItem(feedsTable, 0, 1, false).SetBorder(true)
+	postsFlex.AddItem(postsTable, 0, 1, false).SetBorder(true)
+
+	mainFlex.AddItem(feedsFlex, 0, 1, false)
+	mainFlex.AddItem(postsFlex, 0, 2, false)
 
 	feedsTable.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEscape {
 			app.Stop()
 		}
-	}).SetSelectedFunc(func(row int, column int) {
-		// fmt.Println(table.GetCell(row, column).Text)
-		postsTable.SetCell(1, 0,
-			tview.NewTableCell("si"))
+	}).SetSelectedFunc(func(feedIndex int, column int) {
+		makePostsTable(postsTable, feedIndex)
 	})
 
 	if err := app.SetRoot(mainFlex, true).SetFocus(feedsTable).Run(); err != nil {
