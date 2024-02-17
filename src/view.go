d077fc19ab304165a721e6b9db8102263a0b39df
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-17 13:19:12 -0300

    tview working better

--- src/view.go
+++ src/view.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
@@ -12,6 +10,14 @@ func view(feeds []Feed) {
 	app := tview.NewApplication()
 	table := tview.NewTable().
 		SetSelectable(true, false)
+
+	var mainFlex = tview.NewFlex()
+	var feedsFlex = tview.NewFlex()
+	var postsFlex = tview.NewFlex()
+
+	feedsFlex.AddItem(table, 0, 1, false)
+	feedsFlex.SetBorder(true)
+	mainFlex.AddItem(feedsFlex, 0, 1, false)
 
 	rows := len(feeds)
 	for row := 0; row < rows; row++ {
@@ -26,19 +32,11 @@ func view(feeds []Feed) {
 			app.Stop()
 		}
 	}).SetSelectedFunc(func(row int, column int) {
-		fmt.Println(table.GetCell(row, column).Text)
+		// fmt.Println(table.GetCell(row, column).Text)
+		postsFlex.AddItem(table, 0, 1, false)
+		postsFlex.SetBorder(true)
+		mainFlex.AddItem(postsFlex, 0, 2, false)
 	})
-
-	var mainFlex = tview.NewFlex()
-	var feedsFlex = tview.NewFlex()
-	var postsFlex = tview.NewFlex()
-
-	feedsFlex.AddItem(table, 0, 1, false)
-	feedsFlex.SetBorder(true)
-	postsFlex.AddItem(table, 0, 1, false)
-	postsFlex.SetBorder(true)
-	mainFlex.AddItem(feedsFlex, 0, 1, false)
-	mainFlex.AddItem(postsFlex, 0, 2, false)
 
 	if err := app.SetRoot(mainFlex, true).SetFocus(table).Run(); err != nil {
 		panic(err)
