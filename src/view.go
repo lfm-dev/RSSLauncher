e83686ac9555c3be7bbb552a913a2ac1291251d8
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-17 13:38:57 -0300

    feeds & posts tables

--- src/view.go
+++ src/view.go
@@ -8,37 +8,42 @@ import (
 func view(feeds []Feed) {
 
 	app := tview.NewApplication()
-	table := tview.NewTable().
+	feedsTable := tview.NewTable().
 		SetSelectable(true, false)
 
 	var mainFlex = tview.NewFlex()
 	var feedsFlex = tview.NewFlex()
 	var postsFlex = tview.NewFlex()
 
-	feedsFlex.AddItem(table, 0, 1, false)
+	feedsFlex.AddItem(feedsTable, 0, 1, false)
 	feedsFlex.SetBorder(true)
 	mainFlex.AddItem(feedsFlex, 0, 1, false)
 
+	postsTable := tview.NewTable().SetCell(0, 0,
+		tview.NewTableCell("hola"))
+	postsFlex.AddItem(postsTable, 0, 1, false)
+	postsFlex.SetBorder(true)
+	mainFlex.AddItem(postsFlex, 0, 2, false)
+
 	rows := len(feeds)
 	for row := 0; row < rows; row++ {
-		table.SetCell(row, 0,
+		feedsTable.SetCell(row, 0,
 			tview.NewTableCell(feeds[row].name).
 				SetTextColor(tcell.ColorWhite).
 				SetAlign(tview.AlignLeft))
 	}
 
-	table.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
+	feedsTable.Select(0, 0).SetFixed(1, 1).SetDoneFunc(func(key tcell.Key) {
 		if key == tcell.KeyEscape {
 			app.Stop()
 		}
 	}).SetSelectedFunc(func(row int, column int) {
 		// fmt.Println(table.GetCell(row, column).Text)
-		postsFlex.AddItem(table, 0, 1, false)
-		postsFlex.SetBorder(true)
-		mainFlex.AddItem(postsFlex, 0, 2, false)
+		postsTable.SetCell(1, 0,
+			tview.NewTableCell("si"))
 	})
 
-	if err := app.SetRoot(mainFlex, true).SetFocus(table).Run(); err != nil {
+	if err := app.SetRoot(mainFlex, true).SetFocus(feedsTable).Run(); err != nil {
 		panic(err)
 	}
 
