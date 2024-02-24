95158e47fed94200f61e8693eda5594c05bb5cae
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-24 20:12:19 -0300

    new fn: getSelectedCell

--- src/view_utils.go
+++ src/view_utils.go
@@ -1,16 +1,22 @@
 package main
 
+import "github.com/rivo/tview"
+
+func getSelectedCell(table *tview.Table) *tview.TableCell {
+	cellIndex, _ := table.GetSelection()
+
+	return table.GetCell(cellIndex, 0)
+}
+
 func getFeedData() Feed {
-	feedIndex, _ := feedsTable.GetSelection()
-	cellRef := feedsTable.GetCell(feedIndex, 0).GetReference()
+	cellRef := getSelectedCell(feedsTable).GetReference()
 	feed := cellRef.(Feed)
 
 	return feed
 }
 
 func getItemData() FeedItem {
-	itemIndex, _ := itemsTable.GetSelection()
-	cellRef := itemsTable.GetCell(itemIndex, 0).GetReference()
+	cellRef := getSelectedCell(itemsTable).GetReference()
 	item := cellRef.(FeedItem)
 
 	return item
