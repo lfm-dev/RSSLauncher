8a670a4640e291fc20f2079cb60f0a4316e09d3e
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-05-05 19:04:26 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
-# create .config folder, blacklist.txt, commands.csv and feeds.txt
-# add comments on everyone of those files
+if [ ! -d "$HOME/.config/RSSLauncher" ]; then
+    mkdir "$HOME/.config/RSSLauncher"
+    echo "//write all your blacklisted words, one word per line" > $HOME/.config/RSSLauncher/blacklist.txt
+
+    echo "//write all your commands with the following format:" > $HOME/.config/RSSLauncher/commands.csv
+    echo "//commandName,command (%url will be replaced with the item url)" >> $HOME/.config/RSSLauncher/commands.csv
+    echo "onEnter,firefox %url" >> $HOME/.config/RSSLauncher/commands.csv
+
+    echo "" > $HOME/.config/RSSLauncher/feeds.txt
+fi
